afbda5dfdd8446734c0cc58c76515684ea07c9f7
Chase VanRooyen <cv1438695@gmail.com>
2024-11-08 12:57:19 -0800

    ADC updates

--- PrototypeV2/Core/Src/main.c
+++ PrototypeV2/Core/Src/main.c
@@ -32,7 +32,7 @@
 
 /* Private define ------------------------------------------------------------*/
 /* USER CODE BEGIN PD */
-#define sampleSize 256	//System will capture specificed number of samples per channel
+#define sampleSize 512	//System will capture specificed number of samples per channel
 #define denoiseSize 1
 #define gain 20
 #define devAddress 0x90 //Device address of PCM6260, pre-shift
@@ -114,7 +114,7 @@ PCD_HandleTypeDef hpcd_USB_OTG_HS;
 uint16_t adcGroup1[13 *  sampleSize] = {0}; //buffer for all ADC values connected to ADC1(volume and LR pots)
 uint16_t adcGroup4[2] = {0}; //buffer for all ADC Values connected to ADC4(volume and LR pots)
 uint32_t pcmData[sampleSize  * 8] = {0}; //Buffer for 8 channels of audio data
-int32_t dacData[sampleSize] = {0};
+int32_t dacData[sampleSize * 2] = {0};
 
 
 //contains all instructions for configuring PCM6260
@@ -1330,12 +1330,12 @@ uint16_t getAverageADC(uint16_t position)
 	return temp;
 }
 
-void HAL_ADC_ConvHalfCpltCallback(ADC_HandleTypeDef *hadc)
+void HAL_SAI_RxHalfCpltCallback(SAI_HandleTypeDef *hsai)
 {
 	uint16_t dacSlot = 0;
 	for(int i = 0; i < (sampleSize * 8 ) / 2; i += 8)
 	{
-		float convert = ((((float)pcmData[i] * gain) / 16777216.0f) - 0.5f) * 2.0f;
+		float convert = ((((float)pcmData[i] * (float)gain) / 16777216.0f) - 0.5f) * 2.0f;
 		int32_t output = (int32_t)(convert * 8388608);
 
 		dacData[dacSlot] = output;
@@ -1344,12 +1344,12 @@ void HAL_ADC_ConvHalfCpltCallback(ADC_HandleTypeDef *hadc)
 	}
 }
 
-void HAL_ADC_ConvCpltCallback(ADC_HandleTypeDef *hadc)
+void HAL_SAI_RxCpltCallback(SAI_HandleTypeDef *hsai)
 {
-	uint16_t dacSlot = 0;
-	for(int i = ((sampleSize * 8)) / 2; i < sampleSize; i += 8)
+	uint16_t dacSlot = sampleSize;
+	for(int i = ((sampleSize * 8)) / 2; i < sampleSize * 8; i += 8)
 	{
-		float convert = ((((float)pcmData[i] * gain) / 16777216.0f) - 0.5f) * 2.0f;
+		float convert = ((((float)pcmData[i] * (float)gain) / 16777216.0f) - 0.5f) * 2.0f;
 		int32_t output = (int32_t)(convert * 8388608);
 
 		dacData[dacSlot] = output;
